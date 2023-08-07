07c57dfabb44d07c683ccdb445e0dfd7b2c1b4b8
zbelial <zjytj@qq.com>
2023-08-08 00:26:24 +0800

    fix github issue #1; fix other bugs

--- treesitter-context-c.el
+++ treesitter-context-c.el
@@ -8,7 +8,7 @@
 (defconst treesitter-context--c-query
   '(
     (preproc_if (_) (_) @context.end) @context
-    (preproc_ifdef name: (identifier) (_) @context.end) @context
+    (preproc_ifdef name: (identifier) :anchor (_) @context.end) @context
     (function_definition body: (_) @context.end) @context
     (for_statement body: (_) @context.end) @context
     (if_statement consequence: (_) @context.end) @context
@@ -16,8 +16,9 @@
     (while_statement body: (_) @context.end) @context
     (do_statement body: (_) @context.end) @context
     (switch_statement body: (_) @context.end) @context
-    (case_statement "case" value: (identifier) (_ (_)) @context.end) @context
-    (case_statement "default" (_) @context.end) @context
+    (case_statement value: (identifier) :anchor) @context
+    (case_statement value: (identifier) :anchor (_) @context.end) @context
+    (case_statement !value (_) @context.end) @context
     (struct_specifier body: (_) @context.end) @context
     (enum_specifier body: (_) @context.end) @context    
     )
