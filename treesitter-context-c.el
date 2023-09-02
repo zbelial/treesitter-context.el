d0eac1b98420dc70c0f020f58195a0c09fc0b016
zbelial <zjyzhaojiyang@gmail.com>
2023-09-02 21:55:29 +0800

    fix bug #11

--- treesitter-context-c.el
+++ treesitter-context-c.el
@@ -2,12 +2,20 @@
 
 (require 'treesitter-context-common)
 
-(defconst treesitter-context--c-node-types '("preproc_if" "preproc_ifdef" "function_definition" "for_statement" "if_statement" "else_clause" "while_statement" "do_statement" "struct_specifier" "enum_specifier" "switch_statement" "case_statement")
+(defconst treesitter-context--c-node-types '("preproc_if" "preproc_ifdef" "preproc_else" "function_definition" "for_statement" "if_statement" "else_clause" "while_statement" "do_statement" "struct_specifier" "enum_specifier" "switch_statement" "case_statement")
   "Node types should be showed.")
+
+(defun treesitter-context--c-check-preproc-else-range (node)
+  (let ((start-pos (treesit-node-start node))
+        (end-pos (treesit-node-end node))
+        (point (point)))
+    (and (<= start-pos point)
+         (>= end-pos point))))
 
 (defconst treesitter-context--c-query
   '(
-    (preproc_if (_) (_) @context.end) @context
+    (preproc_if condition: (_) :anchor (_) @context.end) @context
+    (preproc_else (_) @context.end) @context
     (preproc_ifdef name: (identifier) :anchor (_) @context.end) @context
     (function_definition body: (_) @context.end) @context
     (for_statement body: (_) @context.end) @context
@@ -16,9 +24,9 @@
     (while_statement body: (_) @context.end) @context
     (do_statement body: (_) @context.end) @context
     (switch_statement body: (_) @context.end) @context
-    (case_statement value: (identifier) :anchor) @context
-    (case_statement value: (identifier) :anchor (_) @context.end) @context
-    (case_statement !value (_) @context.end) @context
+    (case_statement value: (_) :anchor) @context
+    (case_statement value: (_) :anchor (_) @context.end) @context
+    (case_statement "default" :anchor (_) @context.end) @context
     (struct_specifier body: (_) @context.end) @context
     (enum_specifier body: (_) @context.end) @context)
   "Query patterns to capture desired nodes.")
@@ -29,7 +37,7 @@
 
 (cl-defmethod treesitter-context-indent-context (node context indent-level indent-offset &context (major-mode c-ts-mode))
   (let ((node-type (treesit-node-type node)))
-    (if (member node-type '("else_clause"))
+    (if (member node-type '("else_clause" "preproc_else"))
         (progn
           (setq treesitter-context--indent-level (- indent-level 1))
           (treesitter-context--indent-context context treesitter-context--indent-level indent-offset))
--- treesitter-context-cpp.el
+++ treesitter-context-cpp.el
@@ -2,13 +2,14 @@
 
 (require 'treesitter-context-common)
 
-(defconst treesitter-context--c++-node-types '("preproc_if" "preproc_ifdef" "function_definition" "for_statement" "if_statement" "else_clause" "while_statement" "do_statement" "struct_specifier" "enum_specifier" "for_range_loop" "class_specifier" "linkage_specification" "switch_statement" "case_statement")
+(defconst treesitter-context--c++-node-types '("preproc_if" "preproc_ifdef" "preproc_else" "function_definition" "for_statement" "if_statement" "else_clause" "while_statement" "do_statement" "struct_specifier" "enum_specifier" "for_range_loop" "class_specifier" "linkage_specification" "switch_statement" "case_statement")
   "Node types should be showed.")
 
 (defconst treesitter-context--c++-query
   '(
     (preproc_if (_) (_) @context.end) @context
-    (preproc_ifdef name: (identifier) (_) @context.end) @context
+    (preproc_ifdef name: (identifier) :anchor (_) @context.end) @context
+    (preproc_else (_) @context.end) @context
     (function_definition body: (_) @context.end) @context
     (for_statement (compound_statement) @context.end) @context
     (if_statement consequence: (_) @context.end) @context
@@ -16,8 +17,9 @@
     (while_statement body: (_) @context.end) @context
     (do_statement body: (_) @context.end) @context
     (switch_statement body: (_) @context.end) @context
-    (case_statement "case" value: (identifier) (_ (_)) @context.end) @context
-    (case_statement "default" (_) @context.end) @context
+    (case_statement value: (_) :anchor) @context
+    (case_statement value: (_) :anchor (_) @context.end) @context
+    (case_statement "default" :anchor (_) @context.end) @context
     (struct_specifier body: (_) @context.end) @context
     (enum_specifier body: (_) @context.end) @context    
     (for_range_loop body: (_) @context.end) @context
